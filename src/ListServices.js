9fca4260814c2ef56fd251815c1961686004ae05
Michael Aldave <michael.aldave@outlook.com>
2019-03-02 08:21:43 -0500

    cleanup

--- src/ListServices.js
+++ src/ListServices.js
@@ -1,7 +1,17 @@
-import { makeSoap } from './soap/makeSoap'
-import { updateListItems, getListItems, getListItemsChanges, getList } from './soap/web-services/lists'
-import { encodeXml, listInfoXmlToJson, updateListItemsXmlToJson, getListItemsXmlToJson } from './utils/xml'
-import Caml, { toCaml } from './caml'
+import { makeSoap } from "./soap/makeSoap"
+import {
+  updateListItems,
+  getListItems,
+  getListItemsChanges,
+  getList
+} from "./soap/web-services/lists"
+import {
+  encodeXml,
+  listInfoXmlToJson,
+  updateListItemsXmlToJson,
+  getListItemsXmlToJson
+} from "./utils/xml"
+import Caml, { toCaml } from "./caml"
 
 const DEFAULT_QUERY_OPTIONS = `<QueryOptions>
     <ViewAttributes Scope="RecursiveAll" />
@@ -10,146 +20,130 @@ const DEFAULT_QUERY_OPTIONS = `<QueryOptions>
 </QueryOptions>`
 
 export const connectToList = siteUrl => listName => {
+  listName = encodeXml(listName)
 
-    listName = encodeXml(listName)
-
-    const soapUpdate = batchCmd => items => {
-        const requestOptions = {
-            listName,
-            batchCmd,
-            items: Array.isArray(items) ? items : [items]
-        }
-
-        return makeSoap(siteUrl, updateListItems, requestOptions)
-            .then(updateListItemsXmlToJson)
-            .then(result => Array.isArray(items) ? result : result[0])
+  const soapUpdate = batchCmd => items => {
+    const requestOptions = {
+      listName,
+      batchCmd,
+      items: Array.isArray(items) ? items : [items]
     }
 
-    const soapGet = (select, query, options = {}) => {
+    return makeSoap(siteUrl, updateListItems, requestOptions)
+      .then(updateListItemsXmlToJson)
+      .then(result => (Array.isArray(items) ? result : result[0]))
+  }
 
-        const getOperation = options.withChanges ?
-            getListItemsChanges :
-            getListItems
+  const soapGet = (select, query, options = {}) => {
+    const getOperation = options.withChanges
+      ? getListItemsChanges
+      : getListItems
 
-        if (!Array.isArray(select)) {
-            select = [select]
-        }
-
-        const fieldMap = select.map(selected => {
-            if (typeof selected === 'string') {
-                return {staticName: selected}
-            } else {
-                return selected
-            }
-        })
-
-        const requestOptions = {
-            listName,
-            viewFields: transformSelectedFieldsToViewFields(fieldMap),
-            query,
-            queryOptions: options.queryOptions || DEFAULT_QUERY_OPTIONS,
-            rowLimit: options.rowLimit || 0,
-            ...(options.changeToken && { changeToken: options.changeToken })
-        }
-
-        return makeSoap(siteUrl, getOperation, requestOptions)
-            .then(xml => options.xml ?
-                xml :
-                getListItemsXmlToJson(fieldMap)(xml)
-            )
+    if (!Array.isArray(select)) {
+      select = [select]
     }
 
-    const all = ({ select, orderBy, maxResults }) => {
-        const query = toCaml(Caml.IS_NOT_NULL('ID'), orderBy)
+    const fieldMap = select.map(selected => {
+      if (typeof selected === "string") {
+        return { staticName: selected }
+      } else {
+        return selected
+      }
+    })
 
-        return soapGet(select, query, { rowLimit: maxResults })
+    const requestOptions = {
+      listName,
+      viewFields: transformSelectedFieldsToViewFields(fieldMap),
+      query,
+      queryOptions: options.queryOptions || DEFAULT_QUERY_OPTIONS,
+      rowLimit: options.rowLimit || 0,
+      ...(options.changeToken && { changeToken: options.changeToken })
     }
 
-    const find = ({ select, where, orderBy, maxResults }) => {
-        const query = toCaml(where, orderBy)
+    return makeSoap(siteUrl, getOperation, requestOptions).then(xml =>
+      options.xml ? xml : getListItemsXmlToJson(fieldMap)(xml)
+    )
+  }
 
-        return soapGet(select, query, { rowLimit: maxResults })
+  const all = ({ select, orderBy, maxResults }) => {
+    const query = toCaml(Caml.IS_NOT_NULL("ID"), orderBy)
+
+    return soapGet(select, query, { rowLimit: maxResults })
+  }
+
+  const find = ({ select, where, orderBy, maxResults }) => {
+    const query = toCaml(where, orderBy)
+
+    return soapGet(select, query, { rowLimit: maxResults })
+  }
+
+  const findById = ({ select, id }) => {
+    const query = toCaml(
+      Caml.EQ({ fieldName: "ID", value: id, type: Caml.Types.COUNTER })
+    )
+    return soapGet(select, query).then(([result]) => result)
+  }
+
+  const findOne = ({ select, where }) => {
+    const query = toCaml(where)
+    return soapGet(select, query, { rowLimit: 1 }).then(([result]) => result)
+  }
+
+  const create = soapUpdate("New")
+
+  const updateById = (id, updates) => {
+    const items = { ID: id, ...updates }
+    return soapUpdate("Update")(items)
+  }
+
+  const deleteById = id => {
+    const items = { ID: id }
+    return soapUpdate("Delete")(items)
+  }
+
+  const soapInfo = (options = {}) =>
+    makeSoap(siteUrl, getList, { listName }).then(xml =>
+      options.xml ? xml : listInfoXmlToJson(xml)
+    )
+
+  const count = where => {
+    let resultPromise
+    let selector
+
+    if (where) {
+      const query = toCaml(where)
+      resultPromise = soapGet(["ID"], query, { xml: true })
+      selector = "data"
+    } else {
+      resultPromise = soapInfo({ xml: true })
+      selector = "List"
     }
 
-    const findWithChanges = ({ select, where, changeToken }) => {
-        const query = toCaml(where)
-        return soapGet(select, query, { changeToken, withChanges: true, xml: true })
-    }
+    return resultPromise.then(xml => {
+      const itemCount = xml.querySelector(selector).getAttribute("ItemCount")
+      return itemCount ? parseInt(itemCount) : 0
+    })
+  }
 
-    //TODO: EQ
-    const findById = ({ select, id }) => {
-        const query = toCaml(Caml.EQ({ fieldName: 'ID', value: id, type: Caml.Types.COUNTER }))
-        return soapGet(select, query)
-            .then(([result]) => result)
-    }
+  const getSchema = () => soapInfo()
 
-    // TODO: Return Empty Obj if not found ??
-    const findOne = ({ select, where }) => {
-        const query = toCaml(where)
-        return soapGet(select, query, { rowLimit: 1 })
-            .then(([result]) => result)
-    }
-
-    const create = soapUpdate('New')
-
-    const updateById = (id, updates) => {
-        const items = { ID: id, ...updates }
-        return soapUpdate("Update")(items)
-    }
-
-    const deleteById = id => {
-        const items = { ID: id }
-        return soapUpdate("Delete")(items)
-    }
-
-    const soapInfo = (options = {}) => makeSoap(siteUrl, getList, { listName })
-        .then(xml => options.xml ?
-            xml :
-            listInfoXmlToJson(xml)
-        )
-
-    const count = (where) => {
-        let resultPromise
-        let selector
-
-        if (where) {
-            const query = toCaml(where)
-            resultPromise = soapGet(["ID"], query, { xml: true })
-            selector = "data"
-        }
-        else {
-            resultPromise = soapInfo({ xml: true })
-            selector = "List"
-        }
-
-        return resultPromise
-            .then(xml => {
-                const itemCount = xml.querySelector(selector).getAttribute("ItemCount")
-                return itemCount ? parseInt(itemCount) : 0
-            })
-    }
-
-    const getSchema = () => soapInfo()
-
-    return {
-        getSchema,
-        count,
-        all,
-        find,
-        findWithChanges,
-        findById,
-        findOne,
-        create,
-        updateById,
-        deleteById
-    }
+  return {
+    getSchema,
+    count,
+    all,
+    find,
+    findById,
+    findOne,
+    create,
+    updateById,
+    deleteById
+  }
 }
 
-// TODO: Use with caml helpers
 const transformSelectedFieldsToViewFields = (selectedFields = []) => {
-    const mapFields = ({staticName}) => `<FieldRef Name="${staticName}" />`
+  const mapFields = ({ staticName }) => `<FieldRef Name="${staticName}" />`
 
-    return `<ViewFields Properties="True">
+  return `<ViewFields Properties="True">
         <FieldRef Name="MetaInfo" />
         ${selectedFields.map(mapFields).join("")}
     </ViewFields>`
