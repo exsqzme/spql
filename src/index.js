389f32f094a779238ab5dfc649512de8adbc40aa
Michael Aldave <michael.aldave@outlook.com>
2019-03-02 15:05:06 -0500

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-import { connectToList } from './ListServices'
-import SiteServices from './SiteServices'
-import UserServices from './UserServices'
-import AuthServices from './AuthServices'
-import Caml from './caml'
+import { connectToList } from "./ListServices"
+import SiteServices from "./SiteServices"
+import UserServices from "./UserServices"
+import Caml from "./caml"
 
 const spql = {
-    connect: siteUrl => {
-        const User = UserServices(siteUrl)
-        const List = connectToList(siteUrl)
-        const Auth = AuthServices(User)
-        const Site = SiteServices(siteUrl)
+  connect: siteUrl => {
+    const User = UserServices(siteUrl)
+    const List = connectToList(siteUrl)
+    const Site = SiteServices(siteUrl)
 
-        return {
-            List,
-            Auth,
-            ...Site,
-            ...User
-        }
-    },
-    Caml
-
+    return {
+      List,
+      ...Site,
+      ...User
+    }
+  },
+  Caml
 }
 
 export default spql
